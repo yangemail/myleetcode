b7bc8b56b67d95b50cce13a5e434403502949d63
yangemail8 <yangemail8@gmail.com>
2020-05-26 16:21:19 +0800

    Add #989

--- src/myleetcode/array/easy/Solution989_AddToArrayFormOfInteger.java
+++ src/myleetcode/array/easy/Solution989_AddToArrayFormOfInteger.java
@@ -10,7 +10,7 @@ public class Solution989_AddToArrayFormOfInteger {
 //		int K = 1;
 //		int[] A = new int[] { 0 };
 //		int K = 0;
-		int[] A = new int[] {1,2,6,3,0,7,1,7,1,9,7,5,6,6,4,4,0,0,6,3};
+		int[] A = new int[] { 1, 2, 6, 3, 0, 7, 1, 7, 1, 9, 7, 5, 6, 6, 4, 4, 0, 0, 6, 3 };
 		int K = 516;
 		List<Integer> result = new Solution989_1().addToArrayForm(A, K);
 		System.out.println("Result => " + result);
@@ -20,6 +20,28 @@ public class Solution989_AddToArrayFormOfInteger {
 
 class Solution989_1 {
 	public List<Integer> addToArrayForm(int[] A, int K) {
-		return null;
+		char[] kArray = String.valueOf(K).toCharArray();
+		int carry = 0;
+		int i = A.length - 1;
+		int j = kArray.length - 1;
+		List<Integer> result = new ArrayList<>();
+
+		while (i >= 0 || j >= 0) {
+			int sum = carry;
+			if (i >= 0) {
+				sum += A[i--];
+			}
+			if (j >= 0) {
+				sum += kArray[j--] - '0';
+			}
+			result.add(0, sum % 10);
+			carry = sum / 10;
+		}
+
+		if (carry > 0) {
+			result.add(0, carry);
+		}
+
+		return result;
 	}
 }
