f0c447a0ab0bd43bf8cb62fe9da1b0245a3533d9
yangemail8 <yangemail8@gmail.com>
2020-09-08 21:20:54 +0800

    update #43

--- src/myleetcode/string/medium/Solution43_MutiplyStrings.java
+++ src/myleetcode/string/medium/Solution43_MutiplyStrings.java
@@ -1,5 +1,7 @@
 package myleetcode.string.medium;
 
+import java.util.Arrays;
+
 public class Solution43_MutiplyStrings {
 
 	public static void main(String[] args) {
@@ -8,10 +10,49 @@ public class Solution43_MutiplyStrings {
 		String num1 = "123";
 		String num2 = "456";
 		// expect output = 56088
-		String result = new Solution43_1().multiply(num1, num2);
+		String result = new Solution43_2().multiply(num1, num2);
 		System.out.println("Result => " + result);
 	}
 
+}
+
+class Solution43_2 {
+	public String multiply(String num1, String num2) {
+		if (num1 == null || num1.length() == 0)
+			return num2;
+		if (num2 == null || num2.length() == 0)
+			return num1;
+
+		// multiply without carry handling, save from the end index
+		int[] result = new int[num1.length() + num2.length()];
+		for (int i = num1.length() - 1; i >= 0; i--) {
+			for (int j = num2.length() - 1; j >= 0; j--) {
+				int curr = i + j + 1;
+				int left = curr - 1;
+				result[curr] += toInt(num1, i) * toInt(num2, j);
+				result[left] += result[curr] / 10;
+				result[curr] %= 10;
+			}
+		}
+
+		// calc carry from end of rst
+		StringBuffer sb = new StringBuffer();
+		for (int num : result) {
+			if (!(sb.length() == 0 && num == 0)) { // skip front '0'
+				sb.append(toChar(num));
+			}
+		}
+
+		return sb.length() == 0 ? "0" : sb.toString();
+	}
+
+	private char toChar(int num) {
+		return (char) (num + '0');
+	}
+
+	private int toInt(String s, int x) {
+		return s.charAt(x) - '0';
+	}
 }
 
 class Solution43_1 {
@@ -52,17 +93,17 @@ class Solution43_1 {
 			int number = result[i] % 10;
 			int carrier = result[i] / 10;
 			sb.append(number);
-			if(i < result.length-1) {
-				result[i+1] += carrier; 
+			if (i < result.length - 1) {
+				result[i + 1] += carrier;
 			}
 		}
-		
+
 		sb.reverse();
 		// trim leadding '0's
-		while(sb.length() > 0 && sb.charAt(0) == '0') {
+		while (sb.length() > 0 && sb.charAt(0) == '0') {
 			sb.deleteCharAt(0);
 		}
-		
+
 		return sb.toString();
 	}
 }
